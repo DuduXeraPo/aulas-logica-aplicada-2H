6fdc2b10319d21963893d3a94306dd6228f4600c
rm87410 <rm87410@estudante.fieb.edu.br>
2023-05-04 09:54:24 -0300

    Aula introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int contador = 0;
+        // Aula introdutória de Orientção à Objetos
 
-        while(contador < 10){
-            System.out.println("Contador = " + contador);
-            // incremento
-            contador = contador + 1;
-        }
-
-           // laço para faça
-           for(int x =0; x < 10; x++){
-               System.out.println("contador = " + x);
-           }
-
-           // laço faça enquanto
-        int cont = 1;
-        do{
-            // processo
-            System.out.println(cont);
-            // incremento
-            cont++;
-        }while(cont <= 10);
     }
 }

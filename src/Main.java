57cb6d65cb0db1b04e7de732eeb3e0bbfe50f4ab
rm87410 <rm87410@estudante.fieb.edu.br>
2023-05-18 11:03:43 -0300

    entendi nada

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Aula introdutória de Orientção à Objetos
-
+        // Aula introdutória de Orientação a Objeto
+        // Declaração de objeto
+        Pessoa adao;
+        // Instaciação do objeto
+        adao = new Pessoa();
+        // Ao mesmo tempo
+        Pessoa qualquer = new Pessoa();
+        // Definir formato do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        qualquer.falar();
+        System.out.println(qualquer.falar("oi"));
     }
+
+
 }
